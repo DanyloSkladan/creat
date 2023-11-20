971472fe0c6604b72a80d09b3a36de5c1e37a35f
DanyloSkladan <dskladannnnn09082001@gmail.com>
2023-11-21 01:00:11 +0300

    left

--- html/js/app.js
+++ html/js/app.js
@@ -1,9 +1,38 @@
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
+if (ScrollTrigger.isTouch !==1) {
+
 ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content',
     smooth: 1.5,
     effects: true
 })
+
+
+gsap.fromTo('.hero-section',{opacity: 1 },{
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '1100',
+        scrub: true
+    }
+})
+
+let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+itemsL.forEach(item => {
+
+    gsap.fromTo(item, {x: -200, opacity: 0} , {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            scrub: true
+        }
+    })
+
+})
+
+}
